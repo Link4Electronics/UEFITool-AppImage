31df4b476f3be2527facf7789e997859ffce3f70
Link4Electronics <link4electronics@gmail.com>
2026-04-08 05:49:12 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,6 +9,8 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/LongSoft/UEFITool/refs/heads/new_engine/UEFITool/icons/uefitool_256x256.png
+export DEPLOY_QT=1
+export QT_DIR=qt6
 export ALWAYS_SOFTWARE=1
 
 # Deploy dependencies
